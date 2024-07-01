690b355bba03eb59311fee887b3cd306f52e331d
vigbeamer <vignesh.shanmugavelu@getbeamer.com>
2024-07-01 12:26:54 +0530

    first commit

---
+++ src/global.d.ts
@@ -0,0 +1,6 @@
+declare global {
+  interface Window {
+    userflow: any; // Adjust the type as per your actual use case
+  }
+}
+export {};
---
+++ src/main.tsx
@@ -0,0 +1,82 @@
+import { StrictMode, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import {
+  Outlet,
+  RouterProvider,
+  Link,
+  createRouter,
+  createRoute,
+  createRootRoute,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+const rootRoute = createRootRoute({
+  component: () => {
+    useEffect(() => {
+      window.userflow.init("ct_asy4wrzohrd6podpeziippil3i");
+      window.userflow.setServerEndpoint("userflow.local:4040");
+
+      window.userflow.identify(78651, {
+        email: "alphavignesh98@gmail.com",
+        localeId: "en-US",
+      });
+      console.log({ isIdentified: window.userflow.isIdentified() });
+    }, []);
+
+    return (
+      <>
+        <div className="p-2 flex gap-2">
+          <Link to="/" className="[&.active]:font-bold">
+            Home
+          </Link>{" "}
+          <Link to="/about" className="[&.active]:font-bold">
+            About
+          </Link>
+        </div>
+        <hr />
+        <Outlet />
+        <TanStackRouterDevtools />
+      </>
+    );
+  },
+});
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: function Index() {
+    return (
+      <div className="p-2">
+        <h3>Welcome Home!</h3>
+      </div>
+    );
+  },
+});
+
+const aboutRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/about",
+  component: function About() {
+    return <div className="p-2">Hello from About!</div>;
+  },
+});
+
+const routeTree = rootRoute.addChildren([indexRoute, aboutRoute]);
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+}
